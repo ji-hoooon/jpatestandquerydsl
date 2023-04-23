91ca67e4cab237955ec0ca4d3bcdf6b91a7d0b06
ji-hoooon <ljh6384@naver.com>
2023-04-23 15:57:46 +0900

    ADD init commit

---
+++ src/test/java/com/uno/getinline/repository/EventRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.uno.getinline.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+
+class EventRepositoryTest {
+
+}
